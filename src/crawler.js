eda2e3f23a27919b4b710ab873aa3b8d8d86a8c0
ncouret <nathanael.couret@gmail.com>
2018-08-25 18:02:36 +0200

    More pleasant output

--- src/crawler.js
+++ src/crawler.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const leagues = ["Hurricane", "Typhoon", "Storm", "Gale", "Squall"];
+
 async function crawl() {
     let count = 0;
     for (let l = 0; l < 5; l++) {
@@ -19,7 +21,7 @@ async function crawlDivision(l, d) {
         try {
             const response = await axios.get(`https://clans.worldofwarships.eu/clans/wows/ladder/api/structure/?league=${l}&division=${d}&limit=100&offset=${off}`);
             resultSet = response.data;
-            console.log(`League ${l} Division ${d} page ${off / 100} : ${resultSet.length} results`);
+            if (resultSet.length > 0) console.log(`League ${leagues[l]} Division ${d} page ${(off / 100) + 1} : ${resultSet.length} entries`);
             count += resultSet.length;
             off += 100;
         } catch (e) {
